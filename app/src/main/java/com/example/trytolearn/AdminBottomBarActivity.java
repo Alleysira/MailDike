fa12910cb449a2f19ba3d254d4d475df086b3dfe
Zeiion <36582414+Zeiion@users.noreply.github.com>
2021-04-16 13:33:12 +0800

    change color..

--- app/src/main/java/com/example/trytolearn/AdminBottomBarActivity.java
+++ app/src/main/java/com/example/trytolearn/AdminBottomBarActivity.java
@@ -1,5 +1,7 @@
 package com.example.trytolearn;
 
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,7 @@ public class AdminBottomBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_bar_admin);
 
         BottomNavigationView navView = findViewById(R.id.nav_view_admin);
+        navView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 //        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -24,7 +27,10 @@ public class AdminBottomBarActivity extends AppCompatActivity {
 //                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_admin);
         NavigationUI.setupWithNavController(navView, navController);
-
+        //change color
+//        Resources resource = getResources();
+//        ColorStateList csl = resource.getColorStateList(R.color.lightblue);
+//        navView.setItemTextColor(csl);
     }
 
 
--- app/src/main/java/com/example/trytolearn/UserBottomBarActivity.java
+++ app/src/main/java/com/example/trytolearn/UserBottomBarActivity.java
@@ -1,8 +1,11 @@
 package com.example.trytolearn;
 
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,15 +19,20 @@ public class UserBottomBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_bar);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
-
+        //change color
+//        Resources resource = getResources();
+//        ColorStateList csl = ContextCompat.getColor(context, R.color.blue);
+//        navView.setItemTextColor(csl);
     }
 
 
