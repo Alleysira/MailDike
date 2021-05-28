c545358a3ca1ec95562ea048940a92bfa7a8c000
Alleysira <1367108378@qq.com>
2021-05-28 12:56:12 +0800

    added search bginfo

---
+++ app/src/main/java/com/example/trytolearn/admin_ui/ui/search/SearchFragment.java
@@ -0,0 +1,39 @@
+package com.example.trytolearn.admin_ui.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.trytolearn.R;
+
+public class SearchFragment extends Fragment {
+
+    private SearchViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        notificationsViewModel =
+                new ViewModelProvider(this).get(SearchViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        WebView webView = getActivity().findViewById(R.id.search_back);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.buaa.edu.cn/");
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+}
