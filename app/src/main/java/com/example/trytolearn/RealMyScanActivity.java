dcf44b5212660bec5a3001903c231b7e67a542b9
Alleysira <1367108378@qq.com>
2021-05-30 19:49:34 +0800

    for show

--- app/src/main/java/com/example/trytolearn/RealMyScanActivity.java
+++ app/src/main/java/com/example/trytolearn/RealMyScanActivity.java
@@ -2,7 +2,6 @@ package com.example.trytolearn;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -13,12 +12,15 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.trytolearn.CPABEAPP.CPABE;
 import com.tbruyelle.rxpermissions3.Permission;
 import com.tbruyelle.rxpermissions3.RxPermissions;
 import com.yxing.ScanCodeActivity;
 import com.yxing.ScanCodeConfig;
 import com.yxing.def.ScanStyle;
 
+import cn.edu.buaa.crypto.algebra.serparams.PairingCipherSerParameter;
+import cn.edu.buaa.crypto.algebra.serparams.PairingKeySerParameter;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
@@ -43,9 +45,9 @@ public class RealMyScanActivity extends AppCompatActivity {
         a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent a = new Intent();
                 Toast toast = Toast.makeText(RealMyScanActivity.this, "密文下载成功！", Toast.LENGTH_SHORT);
                 toast.show();
+//                Intent a = new Intent();
 //                a.setClass(RealMyScanActivity.this, TrytocommunicateActivity.class);
 //                startActivity(a);
             }
@@ -159,9 +161,9 @@ public class RealMyScanActivity extends AppCompatActivity {
 
                 String code = extras.getString(ScanCodeConfig.CODE_KEY);
                 //store in the order.xml
-                SharedPreferences.Editor editor = getSharedPreferences("order", MODE_PRIVATE).edit();
-                editor.putString("orderid", code);
-                editor.apply();
+//                SharedPreferences.Editor editor = getSharedPreferences("order", MODE_PRIVATE).edit();
+//                editor.putString("orderid", code);
+//                editor.apply();
 
                 code = deAffine(code);
                 tvCode.setText(String.format("%s%s", "订单号： ", code));
@@ -173,28 +175,26 @@ public class RealMyScanActivity extends AppCompatActivity {
                     public void onClick(View v) {
 
                         // show
-                        String[] out = {"华南陆运枢纽（东莞）", "北京南法信中转场", "海淀学院路速运营业点", "北京航空航天大学", "北京航空航天大学大运村"};
-                        TextView message_view = findViewById(R.id.message_view);
-                        message_view.setText(out[counter % 5]);
-                        counter++;
-
-
-//                        CPABE cpabe = new CPABE();
-////                        String attribute = "Beijing and HD and BUAA";
-//                        String attribute = "116.350" + " and "
-//                                + "9.983";
-//                        String message = "";
-//                        PairingKeySerParameter publicKey = cpabe.getpublickey(default_path_publickey + "pubkey_p", default_path_publickey + "pubkey_g",
-//                                default_path_publickey + "pubkey_h", default_path_publickey + "pubkey_f", default_path_publickey + "pubkey_eggalpha");
-//                        PairingKeySerParameter secretKey = cpabe.getsecretkey(default_path_secretkey + "secretkey_Parameters",
-//                                default_path_secretkey + "secretkey_D", default_path_secretkey + "secretkey_D1s",
-//                                default_path_secretkey + "secretkey_D2s", default_path_secretkey + "secretkey_attributes");
-//                        PairingCipherSerParameter header = cpabe.getheader(default_path_header + "header_Parameters",
-//                                default_path_header + "header_C", default_path_header + "header_C1s",
-//                                default_path_header + "header_C2s", default_path_header + "header_Crhos");
-//                        message = cpabe.Deryption(publicKey, attribute, secretKey, header, default_path_ciphertext + "ciphertext1");
+//                        String[] out = {"华南陆运枢纽（东莞）", "北京南法信中转场", "海淀学院路速运营业点", "北京航空航天大学", "北京航空航天大学大运村"};
 //                        TextView message_view = findViewById(R.id.message_view);
-//                        message_view.setText(message);
+//                        message_view.setText(out[counter % 5]);
+//                        counter++;
+
+                        CPABE cpabe = new CPABE();
+                        String attribute = "116.350" + " and "
+                                + "9.983";
+                        String message = "";
+                        PairingKeySerParameter publicKey = cpabe.getpublickey(default_path_publickey + "pubkey_p", default_path_publickey + "pubkey_g",
+                                default_path_publickey + "pubkey_h", default_path_publickey + "pubkey_f", default_path_publickey + "pubkey_eggalpha");
+                        PairingKeySerParameter secretKey = cpabe.getsecretkey(default_path_secretkey + "secretkey_Parameters",
+                                default_path_secretkey + "secretkey_D", default_path_secretkey + "secretkey_D1s",
+                                default_path_secretkey + "secretkey_D2s", default_path_secretkey + "secretkey_attributes");
+                        PairingCipherSerParameter header = cpabe.getheader(default_path_header + "header_Parameters",
+                                default_path_header + "header_C", default_path_header + "header_C1s",
+                                default_path_header + "header_C2s", default_path_header + "header_Crhos");
+                        message = cpabe.Deryption(publicKey, attribute, secretKey, header, default_path_ciphertext + "ciphertext1");
+                        TextView message_view = findViewById(R.id.message_view);
+                        message_view.setText(message);
                     }
                 });
 
