4d1732cfb6ea2a8a88cf1ff7af1ba9578f6be6c9
Alleysira <1367108378@qq.com>
2021-07-21 17:46:40 +0800

    change getinfo method

--- app/src/main/java/com/example/trytolearn/RealMyScanActivity.java
+++ app/src/main/java/com/example/trytolearn/RealMyScanActivity.java
@@ -20,11 +20,20 @@ import com.yxing.ScanCodeActivity;
 import com.yxing.ScanCodeConfig;
 import com.yxing.def.ScanStyle;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import cn.edu.buaa.crypto.algebra.serparams.PairingCipherSerParameter;
 import cn.edu.buaa.crypto.algebra.serparams.PairingKeySerParameter;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RealMyScanActivity extends AppCompatActivity {
     private static final String default_path_header = "/data/data/com.example.trytolearn/files/";
@@ -46,13 +55,28 @@ public class RealMyScanActivity extends AppCompatActivity {
         a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast toast = Toast.makeText(RealMyScanActivity.this, "密文下载成功！", Toast.LENGTH_SHORT);
-//                toast.show();
-                Intent a = new Intent();
-                a.setClass(RealMyScanActivity.this, TrytocommunicateActivity.class);
-                startActivity(a);
+                Toast toast = Toast.makeText(RealMyScanActivity.this, "开始下载密文！", Toast.LENGTH_SHORT);
+                toast.show();
+//                write2File(getFileRequset(code,"section2"),default_path_ciphertext+"ciphertext1");
+//                write2File(getFileRequset(code,"header_Parameters"),default_path_ciphertext+"header_Parameters");
+//                write2File(getFileRequset(code,"header_C"),default_path_ciphertext+"header_C");
+//                write2File(getFileRequset(code,"header_Crhos"),default_path_ciphertext+"header_Crhos");
+//                write2File(getFileRequset(code,"header_C1s"),default_path_ciphertext+"header_C1s");
+//                write2File(getFileRequset(code,"header_C2s"),default_path_ciphertext+"header_C2s");
+                String location = "116.350" + " and "
+                        + "9.983";
+                write2File(PDAgetFileRequset(code, location, "false", "ciphertext1"), default_path_ciphertext + "ciphertext1");
+                write2File(PDAgetFileRequset(code, location, "false", "header_Parameters"), default_path_ciphertext + "header_Parameters");
+                write2File(PDAgetFileRequset(code, location, "false", "header_C"), default_path_ciphertext + "ciphertext1");
+                write2File(PDAgetFileRequset(code, location, "false", "header_Crhos"), default_path_ciphertext + "header_Crhos");
+                write2File(PDAgetFileRequset(code, location, "false", "header_C1s"), default_path_ciphertext + "header_C1s");
+                write2File(PDAgetFileRequset(code, location, "false", "header_C2s"), default_path_ciphertext + "header_C2s");
+                Toast toast2 = Toast.makeText(RealMyScanActivity.this, "密文下载成功！", Toast.LENGTH_SHORT);
+                toast2.show();
+
             }
         });
+
 
 
         Button tell = findViewById(R.id.call);
@@ -119,6 +143,83 @@ public class RealMyScanActivity extends AppCompatActivity {
     }
 
 
+    public String getFileRequset(String id, String fileName) {
+        final String[] requestdata = {new String("")};
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String ip = "192.168.137.254";
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("id", id)
+                            .add("fileName", fileName)
+                            .build();
+
+                    Request postrequest = new Request.Builder()
+                            .url("http://" + ip + ":8080/code/userGetCode")
+                            .post(requestBody)
+                            .build();
+                    Response response = client.newCall(postrequest).execute();
+                    requestdata[0] = response.body().string();
+//                    SharedPreferences.Editor file = getSharedPreferences(id,MODE_PRIVATE).edit();
+//                    file.putString(fileName,requestdata);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return requestdata[0];
+    }
+
+    public void write2File(String data, String path) {
+        File file = new File(path);
+        try {
+            FileWriter fw = new FileWriter(file, true); //设置成true就是追加
+            fw.write(data);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String PDAgetFileRequset(String id, String location, String isEnd, String fileName) {
+        final String[] requestdata = {""};
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String ip = "192.168.137.254";
+                    OkHttpClient client = new OkHttpClient();
+                    RequestBody requestBody = new FormBody.Builder()
+                            .add("id", id)
+                            .add("location", location)
+                            .add("fileName", fileName)
+                            .add("isEnd", isEnd)
+                            .build();
+
+                    // location needs space
+                    String url = "http://" + ip + ":8080/code/getCode";
+                    Request postrequest = new Request.Builder()
+                            .url(url)
+                            .post(requestBody)
+                            .build();
+                    Response response = client.newCall(postrequest).execute();
+                    requestdata[0] = response.body().string();
+
+//                    SharedPreferences.Editor file = getSharedPreferences(id,MODE_PRIVATE).edit();
+//                    file.putString(fileName,requestdata);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        return requestdata[0];
+    }
+
+
     public String deAffine(String cipher) {
         char[] table = {'.', '?', '@', '^', '*', 'a', '&', '%', '/', '$'};
         char[] c = cipher.toCharArray();
@@ -141,6 +242,11 @@ public class RealMyScanActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK && requestCode == ScanCodeConfig.QUESTCODE && data != null) {
             Bundle extras = data.getExtras();
             if (extras != null) {
+
+                code = extras.getString(ScanCodeConfig.CODE_KEY);
+                code = deAffine(code);
+                tvCode.setText(String.format("%s%s", "订单号： ", code));
+
                 Button tell = findViewById(R.id.call);
                 tell.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -156,9 +262,6 @@ public class RealMyScanActivity extends AppCompatActivity {
                     }
                 });
 
-                code = extras.getString(ScanCodeConfig.CODE_KEY);
-                code = deAffine(code);
-                tvCode.setText(String.format("%s%s", "订单号： ", code));
 
 //              store in the order.xml
                 SharedPreferences.Editor editor = getSharedPreferences("order", MODE_PRIVATE).edit();
